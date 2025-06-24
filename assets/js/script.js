0255c5b39cdfc73a9b66625f1ba8255330bb75bf
foster95 <alicerosefoster95@gmail.com>
2025-06-24 20:16:46 +0100

    Add Fearless question array

--- assets/js/script.js
+++ assets/js/script.js
@@ -86,8 +86,128 @@ const debutQuestions = [
     },
 ]
 
+const fearlessQuestions = [
+    {
+    "question": "You take my hand and drag me head first",
+    "answers": ["Fifteen", "Breathe", "Fearless", "Love Story"],
+    "correct": 2
+    },
+
+    {
+    "question": "And you're dancing 'round your room when the night ends",
+    "answers": ["The Way I Loved You", "The Best Day", "Change", "Fifteen"],
+    "correct": 3
+    },
+
+    {
+    "question": "You'll be the prince and I'll be the princess",
+    "answers": ["Love Story", "White Horse", "Breathe", "Untouchable"],
+    "correct": 0
+    },
+
+    {
+    "question": "I could give you 50 reasons why I should be the one you choose",
+    "answers": ["Change", "Hey Stephen", "Jump Then Fall", "You're Not Sorry"],
+    "correct": 1
+    },
+
+    {
+    "question": "This ain't Hollywood, this is a small town",
+    "answers": ["White Horse", "The Way I Loved You", "Love Story", "Fifteen"],
+    "correct": 0
+    },
+
+    {
+    "question": "And you've got a smile that can light up this whole town",
+    "answers": ["Breathe", "You Belong With Me", "White Horse", "Change"],
+    "correct": 1
+    },
+
+    {
+    "question": "Every little bump in the road I tried to swerve",
+    "answers": ["The Way I Loved You", "Hey Stephen", "Breathe", "The Best Day"],
+    "correct": 2
+    },
+
+    {
+    "question": "Why do you have to put down my dreams so you're the only thing on my mind?",
+    "answers": ["Change", "Tell Me Why", "The Other Side of the Door", "Come in with the Rain"],
+    "correct": 1
+    },
+
+    {
+    "question": "You used to shine so bright but I watched all of it fade ",
+    "answers": ["You're Not Sorry", "Tell Me Why", "The Other Side of the Door", "Come in with the Rain"],
+    "correct": 0
+    },
+
+    {
+    "question": "You used to shine so bright but I watched all of it fade ",
+    "answers": ["You're Not Sorry", "Tell Me Why", "The Other Side of the Door", "Come in with the Rain"],
+    "correct": 0
+    },
+
+    {
+    "question": "You're so in love that you act insane",
+    "answers": ["Fearless", "White Horse", "Change", "The Way I Loved You"],
+    "correct": 3
+    },
+
+    {
+    "question": "Here's to silence, that cuts me to the core",
+    "answers": ["Love Story", "Breathe", "Forever & Always", "Jump then Fall"],
+    "correct": 2
+    },
+
+    {
+    "question": "It's the age of princesses and pirate ships",
+    "answers": ["The Best Day", "Untouchable", "Superstar", "The Other Side of the Door"],
+    "correct": 0
+    },
+
+    {
+    "question": "These walls that they put up to hold us back will fall down",
+    "answers": ["Jump then Fall", "Come in with the Rain", "Change", "Today was a Fairytale"],
+    "correct": 2
+    },
+
+    {
+    "question": "The bottom's gonna drop out from under our feet",
+    "answers": ["Today was a Fairytale", "Jump then Fall", "Superstar", "Love Story"],
+    "correct": 1
+    },
+
+    {
+    "question": "And now that you're close I feel like coming undone",
+    "answers": ["Untouchable", "Breathe", "Fifteen", "White Horse"],
+    "correct": 0
+    },
+
+    {
+    "question": "I could stand up and sing you a song but I don't wanna have to go that far",
+    "answers": ["The Best Day", "Come in with the Rain", "Forever & Always", "You're Not Sorry"],
+    "correct": 1
+    },
+
+    {
+    "question": "So dim that spotlight, tell me things like `I can't take my eyes off of you`",
+    "answers": ["Jump Then Fall", "Untouchable", "You Belong With Me", "Superstar"],
+    "correct": 3
+    },
+
+    {
+    "question": "After everything and that little black dress",
+    "answers": ["The Other Side of the Door", "Forever & Always", "Fifteen", "Hey Stephen"],
+    "correct": 0
+    },
+
+
+
+]
+
 const allErasQuestions = {
     debut: debutQuestions,
+    fearless: fearlessQuestions,
 };
 
 // General Code for the DOM
