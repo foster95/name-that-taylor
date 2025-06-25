da5fc51308c91e775cd64903dbefdfd4b63c271a
foster95 <alicerosefoster95@gmail.com>
2025-06-25 19:40:11 +0100

    Add Speak Now question array

--- assets/js/script.js
+++ assets/js/script.js
@@ -240,9 +240,115 @@ const fearlessQuestions = [
     },
 ];
 
+const speakNowQuestions = [
+
+    {
+    "question": "You learn my secrets and you figure out why I'm guarded",
+    "answers": ["Never Grow Up", "Mine", "Love Story", "Enchanted"],
+    "correct": 1
+    },
+
+    {
+    "question": "Get me with those green eyes, baby",
+    "answers": ["Better than Revenge", "Haunted", "Mean", "Sparks Fly"],
+    "correct": 3
+    },
+
+    {
+    "question": "You gave me all your love and all I gave you was goodbye",
+    "answers": ["Back to December", "The Story of Us", "Mine", "Speak Now"],
+    "correct": 0
+    },
+
+    {
+    "question": "Horrified looks from everyone in the room, but I'm only looking at you",
+    "answers": ["Innocent", "Speak Now", "Mean", "Long Live"],
+    "correct": 1
+    },
+
+    {
+    "question": "And you'll add my name to your long list of traitors",
+    "answers": ["Ours", "Superman", "Dear John", "Better than Revenge"],
+    "correct": 2
+    },
+
+    {
+    "question": "I bet you got pushed around, somebody made you cold",
+    "answers": ["Mean", "Innocent", "Enchanted", "Speak Now"],
+    "correct": 0
+    },
+
+    {
+    "question": "But you held your pride like you should have held me",
+    "answers": ["Long Live", "Ours", "The Story of Us", "Love Story"],
+    "correct": 2
+    },
+
+    {
+    "question": "Remember the footsteps, remember the words said",
+    "answers": ["Better than Revenge", "Never Grow Up", "Sparks Fly", "Mean"],
+    "correct": 1
+    },
+
+    {
+    "question": "The lingering question kept me up 2 AM, who do you love?",
+    "answers": ["Enchanted", "Innocent", "Love Story", "Haunted"],
+    "correct": 0
+    },
+
+    {
+    "question": "I think her ever present frown is a little troubling",
+    "answers": ["Ours", "Mean", "Better than Revenge", "Dear John"],
+    "correct": 2
+    },
+
+    {
+    "question": "Your string of lights is still bright to me"
+    "answers": ["The Story of Us", "Sparks Fly", "Love Story", "Innocent"],
+    "correct": 3
+    },
+
+    {
+    "question": "Stood there and watched you walk away from everything we had"
+    "answers": ["Long Live", "Haunted", "Dear John", "Enchanted"],
+    "correct": 1
+    },
+
+    {
+    "question": "And I hope the sun shines and it's a beautiful day and something reminds you you wish you had stayed "
+    "answers": ["Ours", "If This was a Movie", "Sparks Fly", "Last Kiss"],
+    "correct": 3
+    },
+
+    {
+    "question": "You traded your baseball cap for a crown"
+    "answers": ["Superman", "Long Live", "The Story of Us", "Mean"],
+    "correct": 1
+    },
+
+    {
+    "question": "The stakes are high, the water's rough"
+    "answers": ["Ours", "Love Story", "Innocent", "Back to December"],
+    "correct": 0
+    },
+
+    {
+    "question": "Now I'm pacing down the hall, chasing down your street"
+    "answers": ["Speak Now", "Never Grow Up", "Haunted", "If This was a  Movie"],
+    "correct": 3
+    },
+
+     {
+    "question": "But I hope someday you'll take me away, and save the day"
+    "answers": ["Enchanted", "Ours", "Superman", "Better than Revenge"],
+    "correct": 2
+    },
+]
+
 const allErasQuestions = {
     debut: debutQuestions,
     fearless: fearlessQuestions,
+    speakNow: speakNowQuestions,
 };
 
 // General Code for the DOM
