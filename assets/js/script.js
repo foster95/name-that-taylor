dd672fa0ad2785c91d8a75964a06d5a7a9fe4cba
foster95 <alicerosefoster95@gmail.com>
2025-07-08 10:35:18 +0100

    Add additional questions and red section

--- assets/js/script.js
+++ assets/js/script.js
@@ -116,15 +116,45 @@ const debutQuestions = [
     },
 
     {
-    "question": "He said the way my blue eyes shined Put those Georgia stars to shame that night",
+    "question": "He said the way my blue eyes shined put those Georgia stars to shame that night",
     "answers": ["Invisible", "Tim McGraw", "Tied Together with a Smile", "Picture to Burn"],
+    "correct": 1
+    },
+
+    {
+    "question": "September saw a month of tears, and thankin' God that you weren't here",
+    "answers": ["Our Song", "Invisible", "Tim McGraw", "Stay Beautiful"],
+    "correct": 2
+    },
+
+    {
+    "question": "You need one chance, it was a moment of weakness",
+    "answers": ["Tied Together with a Smile", "Picture to Burn", "Cold as You", "Should Have Said No"],
+    "correct": 3
+    },
+
+    {
+    "question": "I was walkin' up the front porch steps after everything that day",
+    "answers": ["Invisible", "Our Song", "Stay Beautiful", "Tim McGraw"],
+    "correct": 1
+    },
+
+    {
+    "question": "Well, you drive me crazy half the time",
+    "answers": ["Cold as You", "Stay Beautiful", "I'm Only Me when I'm with You", "Invisible"],
+    "correct": 2
+    },
+
+    {
+    "question": "But your eyes still shined like pretty lights",
+    "answers": ["Picture to Burn", "Mary's Song (Oh My My My)", "Teardrops on my Guitar", "Stay Beautiful"],
     "correct": 1
     },
 
 
 ];
 
-// Questions Fearless
+// Questions - Fearless
 
 const fearlessQuestions = [
     {
@@ -234,9 +264,54 @@ const fearlessQuestions = [
     "answers": ["The Other Side of the Door", "Forever & Always", "Fifteen", "Hey Stephen"],
     "correct": 0
     },
+
+    {
+    "question": "You'll be the prince and I'll be the princess",
+    "answers": ["Love Story", "The Way I Loved You", "Breathe", "Fearless"],
+    "correct": 0
+    },
+
+    {
+    "question": "But I miss screaming and fighting and kissing in the rain",
+    "answers": ["Forever & Always", "The Way I Loved You", "The Best Day", "Tell Me Why"],
+    "correct": 1
+    },
+
+    {
+    "question": "It rains when you're here and it rains when you're gone",
+    "answers": ["Hey Stephen", "Fifteen", "Forever & Always", "Change"],
+    "correct": 2
+    },
+
+    {
+    "question": "You're the only thing I know like the back of my hand",
+    "answers": ["Love Story", "Breathe", "White Horse", "Fearless"],
+    "correct": 1
+    },
+
+    {
+    "question": "And we'll sing hallelujah, we'll sing hallelujah",
+    "answers": ["Change", "Fifteen", "You Belong with Me", "Love Story"],
+    "correct": 0
+    },
+
+    {
+    "question": "I'm not a princess, this ain't a fairy tale",
+    "answers": ["Tell me Why", "Fearless", "Jump Then Fall", "White Horse"],
+    "correct": 3
+    },
+
+    {
+    "question": "'Cause I'm too tired at night to call your name",
+    "answers": ["Superstar", "Come in with the Rain", "The Other Side of the Door", "Change"],
+    "correct": 1
+    },
+
+
+
 ];
 
-// Questions Speak Now
+// Questions - Speak Now
 
 const speakNowQuestions = [
 
@@ -336,17 +411,223 @@ const speakNowQuestions = [
     "correct": 3
     },
 
-     {
+    {
     "question": "But I hope someday you'll take me away, and save the day",
     "answers": ["Enchanted", "Ours", "Superman", "Better than Revenge"],
     "correct": 2
     },
+
+    {
+    "question": "Meet me in the pouring rain",
+    "answers": ["Sparks Fly", "Haunted", "Speak Now", "Innocent"],
+    "correct": 0
+    },
+
+    {
+    "question": "You and I walk a fragile line",
+    "answers": ["Last Kiss", "Back to December", "Haunted", "Mean"],
+    "correct": 2
+    },
+
+    {
+    "question": "People throw rocks at things that shine",
+    "answers": ["Love Story", "Ours", "Mean", "Better than Revenge"],
+    "correct": 1
+    },
+
+    {
+    "question": "How the kingdom lights shined just for me and you",
+    "answers": ["Innocent", "Mine", "Long Live", "Speak Now"],
+    "correct": 2
+    },
+
+    {
+    "question": "I still remember the look on your face, lit through the darkness at 1:58",
+    "answers": ["Last Kiss", "Love Story", "Never Grow Up", "Ours"],
+    "correct": 0
+    },
+
+    {
+    "question": "Now I'm searching the room for an empty seat",
+    "answers": ["Long Live", "The Story of Us", "Innocent", "Mean"],
+    "correct": 1
+    },
+
+    {
+    "question": "The girl in the dress, cried the whole way home",
+    "answers": ["Mine", "Sparks Fly", "Back to December", "Dear John"],
+    "correct": 3
+    },
+
+    {
+    "question": "Take pictures in your mind of your childhood room",
+    "answers": ["Innocent", "Ours", "Never Grow Up", "Haunted"],
+    "correct": 2
+    },
+];
+
+// Questions - Red
+
+const redQuestions = [ 
+    
+    {
+    "question": "This is the golden age of something good and right and real",
+    "answers": ["The Last Time", "State of Grace", "All Too Well", "22"],
+    "correct": 1
+    },
+
+    {
+    "question": "Memorizing him was as easy as knowing all the words to your old favorite song",
+    "answers": ["Red", "The Lucky One", "Starlight", "Begin Again"],
+    "correct": 0
+    },
+
+    {
+    "question": "I can't decide if it's a choice, getting swept away",
+    "answers": ["All Too Well", "Sad Beautiful Tragic", "22", "Treacherous"],
+    "correct": 0
+    },
+
+    {
+    "question": "A new notch in your belt is all I'll ever be",
+    "answers": ["I Almost Do", "Stay Stay Stay", "I Knew You Were Trouble", "Holy Ground"],
+    "correct": 2
+    },
+
+    {
+    "question": "But you keep my old scarf from that very first week",
+    "answers": ["The Lucky One", "All Too Well", "22", "I Almost Do"],
+    "correct": 1
+    },
+
+    {
+    "question": "It feels like one of those nights we won't be sleepin'",
+    "answers": ["22", "The Moment I Knew", "The Last Time", "Holy Ground"],
+    "correct": 0
+    },
+
+    {
+    "question": "Oh, we made quite a mess, babe",
+    "answers": ["Sad Beautiful Tragic", "I Almost Do", "Stay Stay Stay", "The Lucky One"],
+    "correct": 1
+    },
+
+    {
+    "question": "But you carry my groceries and now I'm always laughing",
+    "answers": ["Stay Stay Stay", "22", "Treacherous", "All Too Well"],
+    "correct": 0
+    },
+
+    {
+    "question": "Put my name at the top of your list",
+    "answers": ["Everything Has Changed", "The Lucky One", "Red", "The Last Time"],
+    "correct": 3
+    },
+
+    {
+    "question": "Spinning like a girl in a brand new dress",
+    "answers": ["Red", "Holy Ground", "The Moment I Knew", "Stay Stay Stay"],
+    "correct": 1
+    },
+
+    {
+    "question": "Silence, the train runs off its tracks",
+    "answers": ["22", "The Last Time", "Sad Beautiful Tragic", "All Too Well"],
+    "correct": 2
+    },
+
+    {
+    "question": "'Cause you don't feel pretty, you just feel used",
+    "answers": ["The Lucky One", "Stay Stay Stay", "Everything has Changed", "Holy Ground"],
+    "correct": 0
+    },
+
+    {
+    "question": "And all I feel in my stomach is butterflies",
+    "answers": ["Treacherous", "Everything has Changed", "Red", "All Too Well"],
+    "correct": 1
+    },
+
+    {
+    "question": "Like, `Oh my, what a marvelous tune` it was the best night, never would forget how we moved",
+    "answers": ["Red", "22", "Starlight", "Treacherous"],
+    "correct": 2
+    },
+
+    {
+    "question": "I've been spending the last eight months thinking all love ever does is break and burn, and end",
+    "answers": ["Everything has Changed", "Stay Stay Stay", "Holy Ground", "Begin Again"],
+    "correct": 3
+    },
+
+    {
+    "question": "And there in the bathroom I try not to fall apart",
+    "answers": ["The Lucky One", "The Moment I Knew", "22", "All Too Well"],
+    "correct": 1
+    },
+
+    {
+    "question": "This is falling in love in the cruelest way, this is falling for you and you are worlds away",
+    "answers": ["Come Back...Be Here", "The Last Time", "Red", "Treacherous"],
+    "correct": 0
+    },
+
+    {
+    "question": "You understand perfectly, you're the kind of man who makes me sad",
+    "answers": ["22", "Girl at Home", "Red", "All Too Well"],
+    "correct": 1
+    },
+
+    {
+    "question": "Love is a ruthless game unless you play it good and right ",
+    "answers": ["State of Grace", "22", "Holy Ground", "Treacherous"],
+    "correct": 0
+    },
+
+    {
+    "question": "Now I'm lyin' on the cold hard ground",
+    "answers": ["The Last Time", "The Lucky One", "Girl at Home", "I Knew You Were Trouble"],
+    "correct": 3
+    },
+
+    {
+    "question": "Autumn leaves falling down like pieces into place",
+    "answers": ["Holy Ground", "Treacherous", "All Too Well", "Sad Beautiful Tragic"],
+    "correct": 2
+    },
+
+    {
+    "question": "Back to a first glance feeling on New York time",
+    "answers": ["Holy Ground", "The Lucky One", "Stay Stay Stay", "Red"],
+    "correct": 0
+    },
+
+    {
+    "question": "We both wake in lonely beds in different cities",
+    "answers": ["The Last Time", "Sad Beautiful Tragic", "State of Grace", "Treacherous"],
+    "correct": 1
+    },
+
+    {
+    "question": "The whole place was dressed to the nines",
+    "answers": ["Red", "Holy Ground", "Starlight", "Girl at Home"],
+    "correct": 2
+    },
+
+    {
+    "question": "And you throw your head back laughing like a little kid",
+    "answers": ["The Lucky One", "Stay Stay Stay", "I Almost Do", "Begin Again"],
+    "correct": 3
+    },
+
+
 ];
 
 const allErasQuestions = {
     debut: debutQuestions,
     fearless: fearlessQuestions,
-    speakNow: speakNowQuestions
+    speakNow: speakNowQuestions,
+    red: redQuestions
 };
 
 // General Code for the DOM
